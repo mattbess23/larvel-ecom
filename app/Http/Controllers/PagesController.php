554d07e463a82c5f6787d3a554880874edc5a70b
matthew <matthewbessick23@hotmail.com>
2024-03-11 18:17:24 +0200

    Add home and cart function to pages controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    //Home
+    public function home()
+    {
+        echo "Home";
+    }
+
+    //Cart
+    public function cart()
+    {
+        echo "Cart";
+    }
 }
