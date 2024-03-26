57a7f71ef5f18dd63f6e23f0be1b741fb4f783e6
matthew <matthewbessick23@hotmail.com>
2024-03-26 12:37:20 +0200

    fix mix comp

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,12 @@
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
   \*********************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Can't find stylesheet to import.\n  ╷\n1 │ @import 'pages/components/home/_header.sass'\n  │         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n  ╵\n  resources\\sass\\pages\\components\\home\\home.sass 1:9  @import\n  resources\\sass\\app.scss 5:9                         root stylesheet\n    at processResult (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\webpack\\lib\\NormalModule.js:841:19)\n    at D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\webpack\\lib\\NormalModule.js:964:5\n    at D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\loader-runner\\lib\\LoaderRunner.js:400:11\n    at D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\loader-runner\\lib\\LoaderRunner.js:252:18\n    at context.callback (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass-loader\\dist\\index.js:54:7\n    at Function.call$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:117435:16)\n    at render_closure1.call$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:100902:12)\n    at _RootZone.runBinary$3$3 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:37462:18)\n    at _FutureListener.handleError$1 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35978:21)\n    at _Future__propagateToListeners_handleError.call$0 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:36293:49)\n    at Object._Future__propagateToListeners (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:5109:77)\n    at _Future._completeError$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:36144:9)\n    at _AsyncAwaitCompleter.completeError$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35760:12)\n    at Object._asyncRethrow (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:4896:17)\n    at D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:27055:20\n    at _wrapJsFunctionForAsync_closure.$protected (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:4921:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35779:12)\n    at _awaitOnObject_closure0.call$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35773:25)\n    at _RootZone.runBinary$3$3 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:37462:18)\n    at _FutureListener.handleError$1 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35978:21)\n    at _Future__propagateToListeners_handleError.call$0 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:36293:49)\n    at Object._Future__propagateToListeners (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:5109:77)\n    at _Future._completeError$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:36144:9)\n    at _AsyncAwaitCompleter.completeError$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35760:12)\n    at Object._asyncRethrow (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:4896:17)\n    at D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:19809:20\n    at _wrapJsFunctionForAsync_closure.$protected (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:4921:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35779:12)\n    at _awaitOnObject_closure0.call$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35773:25)\n    at _RootZone.runBinary$3$3 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:37462:18)\n    at _FutureListener.handleError$1 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35978:21)\n    at _Future__propagateToListeners_handleError.call$0 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:36293:49)\n    at Object._Future__propagateToListeners (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:5109:77)\n    at _Future._completeError$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:36144:9)\n    at _AsyncAwaitCompleter.completeError$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35760:12)\n    at Object._asyncRethrow (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:4896:17)\n    at D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:19844:20\n    at _wrapJsFunctionForAsync_closure.$protected (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:4921:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35779:12)\n    at _awaitOnObject_closure0.call$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35773:25)\n    at _RootZone.runBinary$3$3 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:37462:18)\n    at _FutureListener.handleError$1 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35978:21)\n    at _Future__propagateToListeners_handleError.call$0 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:36293:49)\n    at Object._Future__propagateToListeners (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:5109:77)\n    at _Future._completeError$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:36144:9)\n    at _AsyncAwaitCompleter.completeError$2 (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:35760:12)\n    at Object._asyncRethrow (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:4896:17)\n    at D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:81013:24\n    at _wrapJsFunctionForAsync_closure.$protected (D:\\Laravel\\Udemy\\udemycommerce\\udemy-ecommerce\\node_modules\\sass\\sass.dart.js:4921:15)");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }),
 
@@ -23,13 +26,144 @@ throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugi
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/js/app": 0,
+/******/ 			"css/app": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	__webpack_modules__["./resources/js/app.js"]();
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./resources/sass/app.scss"]();
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
 ;
